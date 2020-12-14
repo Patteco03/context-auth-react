d0eda351bcbda7707d388913133d47dc41328f37
Thiago <thiagodark1995@gmail.com>
2020-12-13 21:17:40 -0500

    remove imports

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,8 @@
-import React, { useContext } from "react";
-
+import React from "react";
 import { useAuth } from '../contexts/auth';
 import AppRoutes from "./app.routes";
 import AuthRoutes from "./auth.routes";
+
 
 const Routes: React.FC = () => {
   const { signed, loading } = useAuth();
